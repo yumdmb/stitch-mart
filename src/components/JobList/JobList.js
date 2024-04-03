e04c130fd41b16b57d69310fb7781e43e18144cc
yumdmb <elvissawing.muran@gmail.com>
2024-04-03 14:31:19 +0800

    joblist

---
+++ src/components/JobList/JobList.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./JobList.css";
+
+const JobList = () => {
+    return (
+        <ul jobName="job-list">
+        <li>Barista</li>
+        <li>Manager</li>
+        <li>Trainee</li>
+        </ul>
+        );
+}
+
+export default JobList;
