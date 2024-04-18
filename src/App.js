d975715de78090db64e8c966941bd23ca5695eef
yumdmb <elvissawing.muran@gmail.com>
2024-04-19 00:43:43 +0800

    addia

--- src/App.js
+++ src/App.js
@@ -2,12 +2,30 @@ import logo from './logo.svg';
 import './App.css';
 import JobList from './components/JobList/JobList';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Alert, Breadcrumb, Card } from 'react-bootstrap';
+
+
 
 function App() {
   return (
     <div className='Job-list'>
+      <Breadcrumb>
+        <Breadcrumb.Item href='/'>Home</Breadcrumb.Item>
+        <Breadcrumb.Item href='/jobs'>Products</Breadcrumb.Item>
+        <Breadcrumb.Item active>Contact Us</Breadcrumb.Item>
+      </Breadcrumb>      
       <h2>Jobs</h2>
       <JobList />
+      <Alert variant='primary'>You have successfully applied for the job</Alert>
+      <Card>
+        <Card.Header>Job Description</Card.Header>
+        <Card.Body>
+          <Card.Text>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
+            quod.
+          </Card.Text>
+        </Card.Body>
+      </Card>
       <button className='btn btn-primary'>Apply</button>
     </div>
   );
