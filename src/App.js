4e28ce8529127ab9e78c6168a331784f748d1a20
yumdmb <elvissawing.muran@gmail.com>
2024-04-19 00:15:27 +0800

    just for the sake of testing bootstrap here

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import JobList from './components/JobList/JobList';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className='Job-list'>
       <h2>Jobs</h2>
       <JobList />
+      <button className='btn btn-primary'>Apply</button>
     </div>
   );
 }
