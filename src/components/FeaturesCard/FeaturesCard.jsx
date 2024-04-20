5700ce2567c3682ea5ae07380dec0893b9860ecd
adamarbain <u2101735@siswa.um.edu.my>
2024-04-21 02:08:53 +0800

    adjust home page skit boh

--- src/components/FeaturesCard/FeaturesCard.jsx
+++ src/components/FeaturesCard/FeaturesCard.jsx
@@ -1,87 +1,86 @@
 
 const FeaturesCard = () => {  
     return (
-        <div class="px-5 py-5" id="custom-cards" style={{backgroundColor:"#00537A"}}>
-    <h2 class="pb-2 border-bottom text-light">discover the embroidery</h2>
+      <div class="container px-4 py-4" id="custom-cards" style={{backgroundColor:"#00537A"}}>
+        <h2 class="pb-2 border-bottom text-light">discover the embroidery</h2>
 
-    <div class="row row-cols-1 row-cols-lg-3 align-items-stretch g-4 py-5 px-5">
-      
-      <div class="col">
-            <a href="" class="card card-cover h-100 overflow-hidden text-bg-dark rounded-4 shadow-lg" 
+        <div class="row row-cols-1 row-cols-lg-3 align-items-stretch g-4 py-5">
+          <div class="col">
+          <a href="" class="card card-cover h-100 overflow-hidden text-bg-dark rounded-4 shadow-lg" 
             style={{backgroundImage: "url('/embroidery-service.jpg')",
             backgroundSize: 'cover',      
             backgroundPosition: 'center',
             textDecoration: 'none'
             }}>
-                <div class="d-flex flex-column h-100 p-5 pb-3 text-white text-shadow-1">
-                    <h3 class="pt-5 mt-5 mb-4 display-6 lh-1 fw-bold">embroidery service</h3>
-                        <ul class="d-flex list-unstyled mt-auto">
-                        <li class="me-auto">
-                            <div style={{
-                                backgroundColor: 'black',  // Sets the background color to dark
-                                borderRadius: '10px',     // Sets the corners of the div to be rounded
-                                padding: '8px',           // Adds some padding around the image
-                                display: 'inline-block'   // Ensures the div does not stretch unnecessarily
-                            }}>
-                                <img src="horizontal-logo-copy.png" alt="Logo" width="80" height="24" />
-                            </div>
-                        </li>
-                        </ul>
-                </div>
+              <div class="d-flex flex-column h-100 p-5 pb-3 text-white text-shadow-1">
+                <h3 class="pt-5 mt-5 mb-4 display-6 lh-1 fw-bold">embroidery service</h3>
+                <ul class="d-flex list-unstyled mt-auto">
+                  <li class="me-auto">
+                    <div style={{
+                                  backgroundColor: 'black',  // Sets the background color to dark
+                                  borderRadius: '10px',     // Sets the corners of the div to be rounded
+                                  padding: '8px',           // Adds some padding around the image
+                                  display: 'inline-block'   // Ensures the div does not stretch unnecessarily
+                              }}>
+                                  <img src="horizontal-logo-copy.png" alt="Logo" width="80" height="24" />
+                    </div>
+                  </li>
+                </ul>
+              </div>
             </a>
-        </div>
+          </div>
 
-      <div class="col">
-        <a href="" class="card card-cover h-100 overflow-hidden text-bg-dark rounded-4 shadow-lg" 
+          <div class="col">
+          <a href="" class="card card-cover h-100 overflow-hidden text-bg-dark rounded-4 shadow-lg" 
             style={{backgroundImage: "url('/start-embroidery.jpg')",
             backgroundSize: 'cover',      
             backgroundPosition: 'center',
             textDecoration: 'none'
             }}>
-          <div class="d-flex flex-column h-100 p-5 pb-3 text-white text-shadow-1">
-            <h3 class="pt-5 mt-5 mb-4 display-6 lh-1 fw-bold">how to start embroidery</h3>
-            <ul class="d-flex list-unstyled mt-auto">
-              <li class="me-auto">
-                <div style={{
-                    backgroundColor: 'black',  // Sets the background color to dark
-                    borderRadius: '10px',     // Sets the corners of the div to be rounded
-                    padding: '8px',           // Adds some padding around the image
-                    display: 'inline-block'   // Ensures the div does not stretch unnecessarily
-                }}>
-                    <img src="horizontal-logo-copy.png" alt="Logo" width="80" height="24" />
-                </div>
-              </li>
-            </ul>
+              <div class="d-flex flex-column h-100 p-5 pb-3 text-white text-shadow-1">
+                <h3 class="pt-5 mt-5 mb-4 display-6 lh-1 fw-bold">how to start embroidery</h3>
+                <ul class="d-flex list-unstyled mt-auto">
+                  <li class="me-auto">
+                  <div style={{
+                                  backgroundColor: 'black',  // Sets the background color to dark
+                                  borderRadius: '10px',     // Sets the corners of the div to be rounded
+                                  padding: '8px',           // Adds some padding around the image
+                                  display: 'inline-block'   // Ensures the div does not stretch unnecessarily
+                              }}>
+                                  <img src="horizontal-logo-copy.png" alt="Logo" width="80" height="24" />
+                    </div>
+                  </li>
+                </ul>
+              </div>
+            </a>
           </div>
-        </a>
-      </div>
 
-      <div class="col">
-        <a href="" class="card card-cover h-100 overflow-hidden text-bg-dark rounded-4 shadow-lg" 
+          <div class="col">
+          <a href="" class="card card-cover h-100 overflow-hidden text-bg-dark rounded-4 shadow-lg" 
             style={{backgroundImage: "url('/reward.png')",
             backgroundSize: 'cover',      
             backgroundPosition: 'center',
             textDecoration: 'none'
             }}>
-          <div class="d-flex flex-column h-100 p-5 pb-3 text-shadow-1">
-            <h3 class="pt-5 mt-5 mb-4 display-6 lh-1 fw-bold">claim reward and vouchers</h3>
-            <ul class="d-flex list-unstyled mt-auto">
-              <li class="me-auto">
-              <div style={{
-                    backgroundColor: 'black',  // Sets the background color to dark
-                    borderRadius: '10px',     // Sets the corners of the div to be rounded
-                    padding: '8px',           // Adds some padding around the image
-                    display: 'inline-block'   // Ensures the div does not stretch unnecessarily
-                }}>
-                    <img src="horizontal-logo-copy.png" alt="Logo" width="80" height="24" />
-                </div>
-              </li>
-            </ul>
+              <div class="d-flex flex-column h-100 p-5 pb-3 text-shadow-1">
+                <h3 class="pt-5 mt-5 mb-4 display-6 lh-1 fw-bold">claim reward and vouchers</h3>
+                <ul class="d-flex list-unstyled mt-auto">
+                  <li class="me-auto">
+                  <div style={{
+                                  backgroundColor: 'black',  // Sets the background color to dark
+                                  borderRadius: '10px',     // Sets the corners of the div to be rounded
+                                  padding: '8px',           // Adds some padding around the image
+                                  display: 'inline-block'   // Ensures the div does not stretch unnecessarily
+                              }}>
+                                  <img src="horizontal-logo-copy.png" alt="Logo" width="80" height="24" />
+                    </div>
+                  </li>
+                </ul>
+              </div>
+            </a>
           </div>
-        </a>
+        </div>
       </div>
-    </div>
-  </div>
     );
 }
 
--- src/components/HomeContent/HomeContent.jsx
+++ src/components/HomeContent/HomeContent.jsx
@@ -2,29 +2,21 @@ import React from "react";
 
 const HomeContent = () => {
     return (
-        <div style={{
-            backgroundColor: "#FFBA42",
-            height: "70vh", // Use the full height of the viewport
-            display: "flex",
-            alignItems: "center", // This will vertically align the container center
-            justifyContent: "center" // This will horizontally align the container center
-        }}>
-            <div className="container col-xxl-10 px-4 py-2">
-                <div className="row flex-lg-row-reverse align-items-center g-5 py-5">
-                    <div className="col-10 col-sm-8 col-lg-6">
-                        <img src="embroidery.webp" className="d-block mx-lg-auto img-fluid rounded-5" alt="Bootstrap Themes" width="600" height="100" loading="lazy"/>
-                    </div>
-                    <div className="col-lg-6">
-                        <h1 className="display-5 fw-bold text-body-emphasis lh-1 mb-3">Experience the art of embroidery like never before—crafted just for you.</h1>
-                        <p className="lead">Dedicated to perfection, our artisans pour heart and soul into every piece, ensuring each stitch is a testament to timeless beauty.</p>
-                        <div className="d-grid gap-2 d-md-flex justify-content-md-start">
-                            <button type="button" className="btn btn-dark rounded-pill btn-lg px-4 me-md-2">Let's get started</button>
-                        </div>
-                    </div>
+        <div class="container col-xxl-8 px-4 py-5" style={{backgroundColor: "#FFBA42"}}>
+            <div class="row flex-lg-row-reverse align-items-center g-5 py-2">
+            <div class="col-10 col-sm-10 col-lg-6">
+                <img src="embroidery.webp" class="d-block mx-lg-auto img-fluid rounded-5" alt="Bootstrap Themes" width="700" height="500" loading="lazy"/>
+            </div>
+            <div class="col-lg-6">
+                <h1 class="display-5 fw-bold text-body-emphasis lh-1 mb-3">Experience the art of embroidery crafted just for you.</h1>
+                <p class="lead">Dedicated to perfection, our artisans pour heart and soul into every piece, ensuring each stitch is a testament to timeless beauty.</p>
+                <div class="d-grid gap-2 d-md-flex justify-content-md-start">
+                <button type="button" className="btn btn-dark rounded-pill btn-lg px-4 me-md-2">Let's get started</button>
                 </div>
             </div>
+            </div>
         </div>
-    );
+        );
 }
 
 export default HomeContent;
