9f767531e85e88142ca4cd2ae137360e4272afa1
hakim813 <mnurhakimothman@gmail.com>
2024-04-24 12:08:50 +0800

    lawa kan sikit part notification je tadi hehehe

--- src/components/Notification/Notification.jsx
+++ src/components/Notification/Notification.jsx
@@ -13,30 +13,40 @@ function Notification() {
                 <h1>Notifications</h1>
                 <ul className="label-nav">
                     <li>
-                        <p class="bi bi-bell-fill">    All</p>
+                        <button class="btn btn-warning bi bi-bell-fill">    All</button>
                     </li>
                     <li>
-                        <p class="bi bi-journal-x">    Unread</p>
+                        <button class="btn btn-warning bi bi-journal-x">    Unread</button>
                     </li>
                     <li>
-                        <p class="bi bi-basket-fill">    Order Update</p>
+                        <button class="btn btn-warning bi bi-cart-dash-fill">    Order Update</button>
                     </li>
                     <li>
-                        <p class="bi bi-calendar-week">    Appointment</p>
+                        <button class="btn btn-warning bi bi-backpack-fill">    Inventory Alert</button>
+                    </li>
+                    <li>
+                        <button class="btn btn-warning bi bi-calendar-week">    Appointment</button>
                     </li>
                 </ul>
             </div>
 
             <div className='noti-box'>
-                <h1>Unread</h1>
+                <div className='label-in-box'>
+                    <h2><strong>Unread</strong></h2>
+                    <p>Mark all as read</p>
+                </div>
+                
                 <div className='messages'>
-                    <p>Notification 1</p>
+                    <h6 className='bi bi-cart-dash'>     |<strong>                          Order Updates</strong></h6>
+                    <p>Your order is ready to ship!</p>
                 </div>
                 <div className='messages'>
-                    <p>Notification 2</p>
+                    <h6 className='bi bi-calendar'>     |<strong>                          Appointment Updates</strong></h6>
+                    <p><strong>1 day left</strong> before your appointment with us. Can't wait to meet you!</p>
                 </div>
                 <div className='messages'>
-                    <p>Notification 3</p>
+                    <h6 className='bi bi-backpack'>     |<strong>                          Low Inventory Alert</strong></h6>
+                    <p>Your inventory <strong>item #69420</strong> is running low!</p>
                 </div>
             </div>
         </div>
