4b9938bf6f52a2a8c1a9e3aa07db7dfc2d84d632
adamarbain <u2101735@siswa.um.edu.my>
2024-06-08 18:24:04 +0800

    Revert "chore: Refactor SignUp component code style"
    This reverts commit e52e5b929d23c2e4ffdef1e30d8701a2b237e62c.

--- front-end/src/components/Login/Login.jsx
+++ front-end/src/components/Login/Login.jsx
@@ -1,96 +1,75 @@
-import "bootstrap/dist/css/bootstrap.min.css";
-import { useNavigate, Link } from "react-router-dom";
-import "./Login.css";
-import { useState } from "react";
-import {
-  signInStart,
-  signInFailure,
-  signInSuccess,
-} from "../../redux/user/userSlice";
-import { useDispatch, useSelector } from "react-redux";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { useNavigate, Link } from 'react-router-dom';
+import './Login.css';
+import {useState} from 'react';
+import { signInStart, signInFailure, signInSuccess} from '../../redux/user/userSlice'
+import { useDispatch, useSelector } from 'react-redux'
 
 const Login = ({ setUser }) => {
-  const [formData, setFormData] = useState({});
-  const { loading, error } = useSelector((state) => state.user);
-  const navigate = useNavigate();
-  const dispatch = useDispatch();
+    const [formData, setFormData] = useState({});
+    const {loading, error} = useSelector((state) => state.user);
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
 
-  const handleChange = (e) => {
-    setFormData({ ...formData, [e.target.id]: e.target.value });
-  };
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      dispatch(signInStart());
-      const res = await fetch("/api/auth/signin", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(formData),
-        credentials: "include",
-      });
-      const data = await res.json();
-      if (data.success === false) {
-        dispatch(signInFailure(data));
-        return;
-      }
-      dispatch(signInSuccess(data));
-      navigate("/");
-    } catch (error) {
-      dispatch(signInFailure(error));
+    const handleChange = (e) => {
+      setFormData({ ...formData, [e.target.id]: e.target.value });
     }
-  };
-  return (
-    <div className="body">
-      <div className="wrapper">
-        <form onSubmit={handleSubmit} action="">
-          <h1>Login</h1>
-          <div className="input-box">
-            <input
-              id="email"
-              type="text"
-              placeholder="Email"
-              onChange={handleChange}
-            />
-            <i class="bi bi-person-fill"></i>
-          </div>
-          <div className="input-box">
-            <input
-              id="password"
-              type="password"
-              placeholder="Password"
-              onChange={handleChange}
-            />
-            <i class="bi bi-lock-fill"></i>
-          </div>
-          <div className="remember-forgot">
-            <label>
-              <input type="checkbox" />
-              Remember me
-            </label>
-            <a href="">Forgot password?</a>
-          </div>
+    const handleSubmit = async (e) => {
+      e.preventDefault();
+      try {
+        dispatch(signInStart());
+        const res = await fetch('/api/auth/signin',{
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(formData),
+          credentials: 'include'
+        }); 
+        const data = await res.json();
+        if (data.success === false) {
+          dispatch(signInFailure(data));
+          return;
+        }
+        dispatch(signInSuccess(data));
+        navigate('/');
+      } catch (error) {
+        dispatch(signInFailure(error));
+      }
+    }
+    return(
+        <div className='body'>
+        <div className="wrapper">
+            <form onSubmit={handleSubmit} action="">
+                <h1>Login</h1>
+                <div className='input-box'>
+                    <input id='email' type="text" placeholder='Email' onChange={handleChange}  />
+                    <i class="bi bi-person-fill"></i>
+                </div>
+                <div className='input-box'>
+                    <input id='password' type="password" placeholder='Password' onChange={handleChange}  />
+                    <i class="bi bi-lock-fill"></i>
+                </div>
+                <div className='remember-forgot'>
+                    <label><input type="checkbox"/>Remember me</label>
+                    <a href="">Forgot password?</a>
+                </div>
 
-          <button
-            disabled={loading}
-            id="wrapper-button"
-            type="submit"
-            onClick={handleSubmit}
-          >
-            {loading ? "Loading..." : "Sign In"}
-          </button>
+                <button disabled={loading} id='wrapper-button' type='submit' onClick={handleSubmit}>
+                    {loading ? 'Loading...' : 'Sign In'}
+                </button>
 
-          <div className="register-link">
-            <p>Dont have an account?</p>
-            <Link to="/signup">
-              <span>Sign Up</span>
-            </Link>
-          </div>
-        </form>
-      </div>
-    </div>
-  );
+                <div className='register-link'>
+                    <p>Dont have an account?</p>
+                    <Link to="/signup">
+                    <span>Sign Up</span>
+                    </Link>
+                </div>
+            </form>
+        </div>
+        </div>
+    )
+    
 };
 
 export default Login;
--- front-end/src/components/SignUp/SignUp.jsx
+++ front-end/src/components/SignUp/SignUp.jsx
@@ -1,92 +1,79 @@
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./SignUp.css";
-import { useNavigate, Link } from "react-router-dom";
-import { useState } from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './SignUp.css';
+import { useNavigate, Link } from 'react-router-dom';
+import {useState} from 'react';
+
 
 function SignUp() {
-  const [formData, setFormData] = useState({});
-  const [error, setError] = useState(false);
-  const [loading, setLoading] = useState(false);
-  const navigate = useNavigate();
-  const handleChange = (e) => {
-    setFormData({ ...formData, [e.target.id]: e.target.value });
-  };
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      setLoading(true);
-      setError(true);
-      const res = await fetch("http://localhost:3001/api/auth/signup", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(formData),
-      });
-      const data = await res.json();
-      console.log(data);
-      setLoading(false);
-      if (data.success === false) {
-        return;
-      }
-      navigate("/");
-      setError(false);
-    } catch (error) {
-      setLoading(false);
-      setError(true);
+    const [formData, setFormData] = useState({});
+    const [error, setError] = useState(false);
+    const [loading, setLoading] = useState(false);
+    const navigate = useNavigate();
+    const handleChange = (e) => {
+        setFormData({ ...formData, [e.target.id]: e.target.value });
     }
-  };
+    
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+          setLoading(true);
+          setError(true);
+          const res = await fetch('http://localhost:3001/api/auth/signup',{
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(formData),
+          }); 
+          const data = await res.json();
+          console.log(data);
+          setLoading(false);
+          if (data.success === false) {
+            return;
+          }
+          navigate('/');
+          setError(false);
+        } catch (error) {
+          setLoading(false);
+          setError(true);
+        }
+      }
 
-  return (
-    <div className="body">
-      <div className="wrapper">
-        <form onSubmit={handleSubmit}>
-          <h1>Register Account</h1>
-          <div className="input-box">
-            <input
-              id="username"
-              type="text"
-              placeholder="Username"
-              onChange={handleChange}
-            />
-            <i class="bi bi-person-fill"></i>
-          </div>
-          <div className="input-box">
-            <input
-              id="email"
-              type="email"
-              placeholder="Email"
-              onChange={handleChange}
-            />
-            <i class="bi bi-envelope-fill"></i>
-          </div>
-          <div className="input-box">
-            <input
-              id="password"
-              type="password"
-              placeholder="Password"
-              onChange={handleChange}
-            />
-            <i class="bi bi-lock-fill"></i>
-          </div>
-          {/* <div className='remember-forgot'>
+    return(
+        <div className='body'>
+        <div className="wrapper">
+            <form onSubmit={handleSubmit}>
+                <h1>Register Account</h1>
+                <div className='input-box'>
+                    <input id='username' type="text" placeholder='Username' onChange={handleChange}  />
+                    <i class="bi bi-person-fill"></i>
+                </div>
+                <div className='input-box'>
+                    <input id='email' type="email" placeholder='Email' onChange={handleChange}  />
+                    <i class="bi bi-envelope-fill"></i>
+                </div>
+                <div className='input-box'>
+                    <input id='password' type="password" placeholder='Password' onChange={handleChange}  />
+                    <i class="bi bi-lock-fill"></i>
+                </div>
+                {/* <div className='remember-forgot'>
                     <label><input type="checkbox"/>Remember me</label>
                     <a href="">Forgot password?</a>
                 </div> */}
 
-          <button disabled={loading} id="wrapper-button" type="submit">
-            {loading ? "Loading..." : "Sign Up"}
-          </button>
-
-          <div className="register-link">
-            <p>Already have an account?</p>
-            <Link to="/login">Login</Link>
-          </div>
-        </form>
-      </div>
-    </div>
-  );
-}
+                <button disabled={loading} id='wrapper-button' type='submit'>
+                    {loading ? 'Loading...' : 'Sign Up'}
+                </button>
+                
+                <div className='register-link'>
+                    <p>Already have an account?</p>
+                    <Link to='/login'>Login</Link>
+                </div>
+            </form>
+        </div>
+        </div>
+    )
+    
+};
 
 export default SignUp;
