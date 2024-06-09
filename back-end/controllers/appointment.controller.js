727ab68f081dad8e7e73fe7d82557098dce59b79
hakim813 <mnurhakimothman@gmail.com>
2024-06-09 14:09:23 +0800

    auto update noti page when appointment is booked

--- back-end/controllers/appointment.controller.js
+++ back-end/controllers/appointment.controller.js
@@ -1,11 +1,19 @@
 import Appointment from '../models/appointment.model.js';
+import Notification from '../models/notiModel.js';
 
 // Controller to handle appointment booking
 export const createAppointment = async (req, res) => {
     try {
         const { name, email, phone, date, time } = req.body;
         const newAppointment = new Appointment({ name, email, phone, date, time });
+        const newNoti = new Notification({
+                                email: email,
+                                isRead: false, 
+                                category: 'Appointment',
+                                content: `Your appointment has been booked at ${time}, ${date}.`
+                                            });
         await newAppointment.save();
+        await newNoti.save();
         res.status(201).json({ message: 'Appointment booked successfully' });
     } catch (error) {
         res.status(500).json({ message: 'Error booking appointment', error });
