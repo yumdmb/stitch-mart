7a1e33a3c3f0195568b4987964ae99070e2b19fa
yumdmb <elvissawing.muran@gmail.com>
2024-06-11 13:28:57 +0800

    fixing button submit (blum gi ni)

--- front-end/src/components/Inventory/Inventory.jsx
+++ front-end/src/components/Inventory/Inventory.jsx
@@ -80,7 +80,6 @@ function Inventory() {
                         <table>
                             <thead>
                                 <tr>
-                                    <th>Product ID</th>
                                     <th>Product Name</th>
                                     <th>Quantity</th>
                                     <th>Price</th>
@@ -125,68 +124,6 @@ function Inventory() {
                     </div>
                 </div>
 
-                {/* Supplier List (No changes needed) */}
-                <div className='supplier-list'>
-                    <div className='supplier-list-header'>
-                        <h2>Supplier List</h2> 
-                    </div>
-                    <div className='inv-table'>
-                        <table>
-                            <thead>
-                                <tr>
-                                    <th>Supplier Name</th>
-                                    <th>Address</th>
-                                    <th>Email</th>
-                                    <th>Contact No.</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <td>Kayangan Supplies Sdn Bhd</td>
-                                    <td>33, jalan 1, 02145 Pasir Salak, Johor</td>
-                                    <td>kayangan@gmail.com</td>
-                                    <td>0377708656</td>
-                                </tr>
-                                <tr>
-                                    <td>Abu Embroidery Supplies</td>
-                                    <td>No. 1, Jalan 14C, 56810 Setapak, Pulau Pinang</td>
-                                    <td>abuembroi@gmail.com</td>
-                                    <td>0129004422</td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </div>
-
-                    <div className="add-supplier">
-                        <div className="add-supplier-header">
-                            <h4>Add Supplier</h4>
-                        </div>
-                        <form>
-                            <div className="form-row">
-                                <div className="form-group">
-                                    <label htmlFor="supplierName">Supplier Name:</label>
-                                    <input type="text" id="supplierName" name="supplierName" required />
-                                </div>
-                                <div className="form-group">
-                                    <label htmlFor="address">Address:</label>
-                                    <input type="text" id="address" name="address" required />
-                                </div>
-                            </div>
-                            <div className="form-row">
-                                <div className="form-group">
-                                    <label htmlFor="email">Email:</label>
-                                    <input type="email" id="email" name="email" required />
-                                </div>
-                                <div className="form-group">
-                                    <label htmlFor="contactno">Contact No:</label>
-                                    <input type="tel" id="contactno" name="contactno" required />
-                                </div>
-                            </div>
-                            
-                            <button type="submit">Add Supplier</button>
-                        </form>
-                    </div>
-                </div>
             </div>
         </div>
     );
