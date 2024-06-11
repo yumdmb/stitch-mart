97b9b252ff859bd2465791136bca6891690588e3
adamarbain <u2101735@siswa.um.edu.my>
2024-06-11 13:17:04 +0800

    feat: add notification button to header

--- front-end/src/components/Header/Header.jsx
+++ front-end/src/components/Header/Header.jsx
@@ -109,6 +109,33 @@ export default function Header() {
                   </Link>
                 </li>
 
+                {currentUser ? (
+                    <li>
+                    <button
+                      className="nav-link d-flex flex-column align-items-center justify-content-center text-white"
+                      onClick={handleNotiClick}
+                    >
+                      <div>
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="24"
+                          height="24"
+                          fill="#FFBA42"
+                          className="bi bi-bell-fill"
+                          viewBox="0 0 16 16"
+                        >
+                          <path d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2m.995-14.901a1 1 0 1 0-1.99 0A5 5 0 0 0 3 6c0 1.098-.5 6-2 7h14c-1.5-1-2-5.902-2-7 0-2.42-1.72-4.44-4.005-4.901" />
+                        </svg>
+                      </div>
+                      <div>Notification</div>
+                    </button>
+                  </li>
+                  ) : (
+                    <li>
+
+                    </li>
+                  )}
+
                 {isAdmin && (
                   <>
                     <li>
@@ -179,27 +206,6 @@ export default function Header() {
                     <div>Inventory</div>
                   </button>
                 </li> */}
-
-                <li>
-                  <button
-                    className="nav-link d-flex flex-column align-items-center justify-content-center text-white"
-                    onClick={handleNotiClick}
-                  >
-                    <div>
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="24"
-                        height="24"
-                        fill="#FFBA42"
-                        className="bi bi-bell-fill"
-                        viewBox="0 0 16 16"
-                      >
-                        <path d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2m.995-14.901a1 1 0 1 0-1.99 0A5 5 0 0 0 3 6c0 1.098-.5 6-2 7h14c-1.5-1-2-5.902-2-7 0-2.42-1.72-4.44-4.005-4.901" />
-                      </svg>
-                    </div>
-                    <div>Notification</div>
-                  </button>
-                </li>
 
                 {/* <li>
                   <button
